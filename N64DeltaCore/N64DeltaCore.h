aacc969760df4e72cd9ce10719b54d68c9064180
Riley Testut <riley@rileytestut.com>
2019-03-27 11:18:17 -0700

    Initial Commit

---
+++ N64DeltaCore/N64DeltaCore.h
@@ -0,0 +1,19 @@
+//
+//  N64DeltaCore.h
+//  N64DeltaCore
+//
+//  Created by Riley Testut on 3/27/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for N64DeltaCore.
+FOUNDATION_EXPORT double N64DeltaCoreVersionNumber;
+
+//! Project version string for N64DeltaCore.
+FOUNDATION_EXPORT const unsigned char N64DeltaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <N64DeltaCore/PublicHeader.h>
+
+
