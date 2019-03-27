9c17f27e5e7e823cbdabee83e5bb20501e6cdde4
Riley Testut <riley@rileytestut.com>
2019-03-27 15:39:47 -0700

    Adds GLideN64 plugin files to N64DeltaCore-Video target
    Fixes compilation error

---
+++ N64DeltaCore-Video/N64DeltaCore_Video.h
@@ -0,0 +1,19 @@
+//
+//  N64DeltaCore_Video.h
+//  N64DeltaCore-Video
+//
+//  Created by Riley Testut on 3/27/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for N64DeltaCore_Video.
+FOUNDATION_EXPORT double N64DeltaCore_VideoVersionNumber;
+
+//! Project version string for N64DeltaCore_Video.
+FOUNDATION_EXPORT const unsigned char N64DeltaCore_VideoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <N64DeltaCore_Video/PublicHeader.h>
+
+
