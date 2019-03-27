34145a0d1ad01a0643dade7dc2a43585576f4f6c
Riley Testut <riley@rileytestut.com>
2019-03-27 12:53:14 -0700

    Adds cxd4 plugin files to N64DeltaCore-RSP target

---
+++ N64DeltaCore-RSP/N64DeltaCore_RSP.h
@@ -0,0 +1,19 @@
+//
+//  N64DeltaCore_RSP.h
+//  N64DeltaCore-RSP
+//
+//  Created by Riley Testut on 3/27/19.
+//  Copyright © 2019 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for N64DeltaCore_RSP.
+FOUNDATION_EXPORT double N64DeltaCore_RSPVersionNumber;
+
+//! Project version string for N64DeltaCore_RSP.
+FOUNDATION_EXPORT const unsigned char N64DeltaCore_RSPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <N64DeltaCore_RSP/PublicHeader.h>
+
+
