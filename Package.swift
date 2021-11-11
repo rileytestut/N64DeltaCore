ba000f276d1227ae732350f1469897740e312dd5
Riley Testut <riley@rileytestut.com>
2021-11-10 16:00:13 -0800

    Fixes missing libpng symbols

--- Package.swift
+++ Package.swift
@@ -225,6 +225,23 @@ let package = Package(
                 "plugin_delta.c",
                 "TxDbg_ios.mm",
                 "GLideN64/src",
+                
+                "libpng/png.c",
+                "libpng/pngerror.c",
+                "libpng/pngget.c",
+                "libpng/pngmem.c",
+                "libpng/pngpread.c",
+                "libpng/pngread.c",
+                "libpng/pngrio.c",
+                "libpng/pngrtran.c",
+                "libpng/pngrutil.c",
+                "libpng/pngset.c",
+                "libpng/pngtest.c",
+                "libpng/pngtrans.c",
+                "libpng/pngwio.c",
+                "libpng/pngwrite.c",
+                "libpng/pngwtran.c",
+                "libpng/pngwutil.c"
             ],
             cSettings: [
                 .headerSearchPath("GLideN64/src"),
