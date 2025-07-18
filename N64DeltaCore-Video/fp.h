0d1372b85cac01d2a151d3a2d9015af41f051943
Riley Testut <riley@rileytestut.com>
2025-07-18 16:07:51 -0500

    Fixes compiling with Xcode 26

---
+++ N64DeltaCore-Video/fp.h
@@ -0,0 +1,16 @@
+//
+//  fp.h
+//  N64DeltaCore
+//
+//  Created by Riley Testut on 7/18/25.
+//  Copyright © 2025 Riley Testut. All rights reserved.
+//
+
+#ifndef fp_h
+#define fp_h
+
+// Effectively replaces "#include <fp.h>" with "#include <math.h>".
+// Fixes compiling with Xcode 26.
+#include <math.h>
+
+#endif /* fp_h */
